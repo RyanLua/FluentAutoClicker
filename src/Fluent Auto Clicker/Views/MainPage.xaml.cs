50f609b79fa5c87a755287c1713c1c9eaaa502bd
Ryan Lua <80087248+RyanLua@users.noreply.github.com>
2023-07-07 13:56:12 -0700

    UI adjustments

--- src/Fluent Auto Clicker/Views/MainPage.xaml.cs
+++ src/Fluent Auto Clicker/Views/MainPage.xaml.cs
@@ -2,6 +2,7 @@
 using Fluent_Auto_Clicker.Helpers;
 using Microsoft.UI.Xaml;
 using Microsoft.UI.Xaml.Controls;
+using Microsoft.UI.Xaml.Controls.Primitives;
 using Windows.Globalization.NumberFormatting;
 
 namespace Fluent_Auto_Clicker.Views;
