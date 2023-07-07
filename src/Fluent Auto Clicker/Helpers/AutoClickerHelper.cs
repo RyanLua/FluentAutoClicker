6429dadee78daed616476ded5f7017c958a99344
Ryan Lua <80087248+RyanLua@users.noreply.github.com>
2023-07-06 20:12:59 -0700

    Add mouse button support

--- src/Fluent Auto Clicker/Helpers/AutoClickerHelper.cs
+++ src/Fluent Auto Clicker/Helpers/AutoClickerHelper.cs
@@ -2,59 +2,76 @@
 using System.Runtime.InteropServices;
 using System.Threading;
 
-namespace Fluent_Auto_Clicker.Helpers
+namespace Fluent_Auto_Clicker.Helpers;
+
+public static class AutoClickerHelper
 {
-    public static class AutoClickerHelper
+    [DllImport("user32.dll", CharSet = CharSet.Auto, CallingConvention = CallingConvention.StdCall)]
+    public static extern void mouse_event(uint dwFlags, uint dx, uint dy, uint cButtons, uint dwExtraInfo);
+
+    private const int MOUSEEVENTF_LEFTDOWN = 0x0002;
+    private const int MOUSEEVENTF_LEFTUP = 0x0004;
+    private const int MOUSEEVENTF_MIDDLEDOWN = 0x0020;
+    private const int MOUSEEVENTF_MIDDLEUP = 0x0040;
+    private const int MOUSEEVENTF_RIGHTDOWN = 0x0008;
+    private const int MOUSEEVENTF_RIGHTUP = 0x0010;
+
+    private static bool _isAutoClickerRunning;
+    private static Thread _autoClickerThread;
+
+    public static int RepeatInterval = 1000;
+    public static int RepeatCount = 0;
+    public static int MouseButton = 0; // 0 = Left, 1 = Middle, 2 = Right
+
+    public static bool IsAutoClickerRunning => _isAutoClickerRunning;
+
+    public static void StartAutoClicker()
     {
-        [DllImport("user32.dll", CharSet = CharSet.Auto, CallingConvention = CallingConvention.StdCall)]
-        public static extern void mouse_event(uint dwFlags, uint dx, uint dy, uint cButtons, uint dwExtraInfo);
-
-        private const int MOUSEEVENTF_LEFTDOWN = 0x02;
-        private const int MOUSEEVENTF_LEFTUP = 0x04;
-
-        private static bool _isAutoClickerRunning;
-        private static Thread _autoClickerThread;
-
-        public static int RepeatInterval = 1000;
-        public static int RepeatCount = 0;
-
-        public static bool IsAutoClickerRunning => _isAutoClickerRunning;
-
-        public static void StartAutoClicker()
+        if (!_isAutoClickerRunning)
         {
-            if (!_isAutoClickerRunning)
-            {
-                _isAutoClickerRunning = true;
-                _autoClickerThread = new Thread(AutoClickerThread);
-                _autoClickerThread.Start();
-            }
+            _isAutoClickerRunning = true;
+            _autoClickerThread = new Thread(AutoClickerThread);
+            _autoClickerThread.Start();
         }
+    }
 
-        public static void StopAutoClicker()
+    public static void StopAutoClicker()
+    {
+        if (_isAutoClickerRunning)
         {
-            if (_isAutoClickerRunning)
-            {
-                _isAutoClickerRunning = false;
-                _autoClickerThread.Join();
-            }
+            _isAutoClickerRunning = false;
+            _autoClickerThread.Join();
         }
+    }
 
-        private static void AutoClickerThread()
+    private static void AutoClickerThread()
+    {
+        var clickCount = 0;
+
+        while (_isAutoClickerRunning && (RepeatCount <= 0 || clickCount < RepeatCount))
         {
-            int clickCount = 0;
-
-            while (_isAutoClickerRunning && (RepeatCount <= 0 || clickCount < RepeatCount))
+            if (MouseButton == 0)
             {
                 mouse_event(MOUSEEVENTF_LEFTDOWN, 0, 0, 0, 0);
                 mouse_event(MOUSEEVENTF_LEFTUP, 0, 0, 0, 0);
-                
-                if (RepeatCount > 0)
-                {
-                    clickCount++;
-                }
-
-                Thread.Sleep(RepeatInterval);
             }
+            else if (MouseButton == 1)
+            {
+                mouse_event(MOUSEEVENTF_MIDDLEDOWN, 0, 0, 0, 0);
+                mouse_event(MOUSEEVENTF_MIDDLEUP, 0, 0, 0, 0);
+            }
+            else
+            {
+                mouse_event(MOUSEEVENTF_RIGHTDOWN, 0, 0, 0, 0);
+                mouse_event(MOUSEEVENTF_RIGHTUP, 0, 0, 0, 0);
+            }
+
+            if (RepeatCount > 0)
+            {
+                clickCount++;
+            }
+
+            Thread.Sleep(RepeatInterval);
         }
     }
 }
--- src/Fluent Auto Clicker/Views/MainPage.xaml.cs
+++ src/Fluent Auto Clicker/Views/MainPage.xaml.cs
@@ -4,77 +4,86 @@ using Microsoft.UI.Xaml;
 using Microsoft.UI.Xaml.Controls;
 using Windows.Globalization.NumberFormatting;
 
-namespace Fluent_Auto_Clicker.Views
+namespace Fluent_Auto_Clicker.Views;
+
+public sealed partial class MainPage : Page
 {
-    public sealed partial class MainPage : Page
+    public MainPage()
     {
-        public MainPage()
+        InitializeComponent();
+        StartClicker.Checked += StartClicker_Checked;
+        StartClicker.Unchecked += StartClicker_Unchecked;
+    }
+
+    private void ButtonType_SelectionChanged(object sender, SelectionChangedEventArgs e)
+    {
+        var comboBox = (ComboBox)sender;
+        var mouseButtonIndex = comboBox.SelectedIndex;
+
+        AutoClickerHelper.MouseButton = mouseButtonIndex;
+
+        Debug.WriteLine($"Mouse Button: {AutoClickerHelper.MouseButton}");
+    }
+
+    private void SetClicker_Interval()
+    {
+        var hours = Convert.ToInt32(Interval_Hours.Value);
+        var minutes = Convert.ToInt32(Interval_Minutes.Value);
+        var seconds = Convert.ToInt32(Interval_Seconds.Value);
+        var milliseconds = Convert.ToInt32(Interval_Milliseconds.Value);
+
+        var totalTimeInMilliseconds = ((hours * 60 + minutes) * 60 + seconds) * 1000 + milliseconds;
+
+        AutoClickerHelper.RepeatInterval = totalTimeInMilliseconds;
+
+        Debug.WriteLine($"Interval Time (Milliseconds): {AutoClickerHelper.RepeatInterval}");
+    }
+
+    private void IntervalNumberBox_ValueChanged(NumberBox sender, NumberBoxValueChangedEventArgs args)
+    {
+        SetClicker_Interval();
+    }
+
+    private void RepeatTypeRadioButtons_SelectionChanged(object sender, RoutedEventArgs e)
+    {
+        SetClicker_Repeat();
+    }
+
+    private void RepeatTypeNumberBox_ValueChanged(NumberBox sender, NumberBoxValueChangedEventArgs args)
+    {
+        SetClicker_Repeat();
+    }
+
+    private void SetClicker_Repeat()
+    {
+        var repeatCount = Convert.ToInt32(RepeatType_Clicks.Value);
+        var repeatTypeIndex = RepeatTypeRadioButtons.SelectedIndex;
+
+        if (repeatTypeIndex == 0)
         {
-            InitializeComponent();
-            StartClicker.Checked += StartClicker_Checked;
-            StartClicker.Unchecked += StartClicker_Unchecked;
+            AutoClickerHelper.RepeatCount = repeatCount;
+        }
+        else
+        {
+            AutoClickerHelper.RepeatCount = 0;
         }
 
-        private void SetClicker_Interval()
-        {
-            var hours = Convert.ToInt32(Interval_Hours.Value);
-            var minutes = Convert.ToInt32(Interval_Minutes.Value);
-            var seconds = Convert.ToInt32(Interval_Seconds.Value);
-            var milliseconds = Convert.ToInt32(Interval_Milliseconds.Value);
-
-            var totalTimeInMilliseconds = ((hours * 60 + minutes) * 60 + seconds) * 1000 + milliseconds;
-
-            AutoClickerHelper.RepeatInterval = totalTimeInMilliseconds;
-
-            Debug.WriteLine($"Interval Time (Milliseconds): {AutoClickerHelper.RepeatInterval}");
-        }
-
-        private void IntervalNumberBox_ValueChanged(NumberBox sender, NumberBoxValueChangedEventArgs args)
-        {
-            SetClicker_Interval();
-        }
-
-        private void RepeatTypeRadioButtons_SelectionChanged(object sender, RoutedEventArgs e)
-        {
-            SetClicker_Repeat();
-        }
-
-        private void RepeatTypeNumberBox_ValueChanged(NumberBox sender, NumberBoxValueChangedEventArgs args)
-        {
-            SetClicker_Repeat();
-        }
-
-        private void SetClicker_Repeat()
-        {
-            var repeatCount = Convert.ToInt32(RepeatType_Clicks.Value);
-            var repeatTypeIndex = RepeatTypeRadioButtons.SelectedIndex;
-
-            if (repeatTypeIndex == 0)
-            {
-                AutoClickerHelper.RepeatCount = repeatCount;
-            }
-            else
-            {
-                AutoClickerHelper.RepeatCount = 0;
-            }
-
-            Debug.WriteLine($"Repeat Count: {AutoClickerHelper.RepeatCount}");
-        }
+        Debug.WriteLine($"Repeat Count: {AutoClickerHelper.RepeatCount}");
+    }
 
 
-        private void StartClicker_Checked(object sender, RoutedEventArgs e)
-        {
-            AutoClickerHelper.StartAutoClicker();
-            SetClicker_Interval();
-            SetClicker_Repeat();
+    private void StartClicker_Checked(object sender, RoutedEventArgs e)
+    {
+        AutoClickerHelper.StartAutoClicker();
+        SetClicker_Interval();
+        SetClicker_Repeat();
 
-            Debug.WriteLine($"Auto Clicker Running: {AutoClickerHelper.IsAutoClickerRunning}");
-        }
+        Debug.WriteLine($"Auto Clicker Running: {AutoClickerHelper.IsAutoClickerRunning}");
+    }
 
-        private void StartClicker_Unchecked(object sender, RoutedEventArgs e)
-        {
-            //AutoClickerHelper.StopAutoClicker();
-            Debug.WriteLine($"Auto Clicker Stopped: {AutoClickerHelper.IsAutoClickerRunning}");
-        }
+    private void StartClicker_Unchecked(object sender, RoutedEventArgs e)
+    {
+        //AutoClickerHelper.StopAutoClicker();
+        Debug.WriteLine($"Auto Clicker Stopped: {AutoClickerHelper.IsAutoClickerRunning}");
     }
 }
