06f57aeb5e678063c51f0fadac09673115f8690a
RyanLua <80087248+RyanLua@users.noreply.github.com>
2023-08-13 16:17:08 -0700

    Convert mouse_event to SendInput

--- src/Fluent Auto Clicker/Helpers/AutoClickerHelper.cs
+++ src/Fluent Auto Clicker/Helpers/AutoClickerHelper.cs
@@ -2,76 +2,101 @@
 using System.Runtime.InteropServices;
 using System.Threading;
 
-namespace Fluent_Auto_Clicker.Helpers;
-
-public static class AutoClickerHelper
+namespace Fluent_Auto_Clicker.Helpers
 {
-    [DllImport("user32.dll", CharSet = CharSet.Auto, CallingConvention = CallingConvention.StdCall)]
-    public static extern void mouse_event(uint dwFlags, uint dx, uint dy, uint cButtons, uint dwExtraInfo);
-
-    private const int MOUSEEVENTF_LEFTDOWN = 0x0002;
-    private const int MOUSEEVENTF_LEFTUP = 0x0004;
-    private const int MOUSEEVENTF_MIDDLEDOWN = 0x0020;
-    private const int MOUSEEVENTF_MIDDLEUP = 0x0040;
-    private const int MOUSEEVENTF_RIGHTDOWN = 0x0008;
-    private const int MOUSEEVENTF_RIGHTUP = 0x0010;
-
-    private static bool _isAutoClickerRunning;
-    private static Thread _autoClickerThread;
-
-    public static int clickInterval = 1000; // Milliseconds
-    public static int repeatAmount = 0; // How many clicks
-    public static int mouseButton = 0; // 0 = Left, 1 = Middle, 2 = Right
-
-    public static bool IsAutoClickerRunning => _isAutoClickerRunning;
-
-    public static void StartAutoClicker()
+    public static class AutoClickerHelper
     {
-        _isAutoClickerRunning = true;
-        _autoClickerThread = new Thread(AutoClickerThread);
-        _autoClickerThread.Start();
-    }
+        [DllImport("user32.dll", SetLastError = true)]
+        private static extern uint SendInput(uint nInputs, INPUT[] pInputs, int cbSize);
 
-    public static void StopAutoClicker()
-    {
-        _isAutoClickerRunning = false;
-        _autoClickerThread.Join();
-    }
-
-    private static void AutoClickerThread()
-    {
-        var clickCount = 0;
-
-        while (_isAutoClickerRunning == true)
+        [StructLayout(LayoutKind.Sequential)]
+        private struct INPUT
         {
-            if (clickCount >= repeatAmount && repeatAmount != 0)
-            {
-                StopAutoClicker();
-                break;
-            }
+            public int type;
+            public MOUSEINPUT mi;
+        }
 
-            if (mouseButton == 0)
-            {
-                mouse_event(MOUSEEVENTF_LEFTDOWN, 0, 0, 0, 0);
-                mouse_event(MOUSEEVENTF_LEFTUP, 0, 0, 0, 0);
-            }
-            else if (mouseButton == 1)
-            {
-                mouse_event(MOUSEEVENTF_MIDDLEDOWN, 0, 0, 0, 0);
-                mouse_event(MOUSEEVENTF_MIDDLEUP, 0, 0, 0, 0);
-            }
-            else
-            {
-                mouse_event(MOUSEEVENTF_RIGHTDOWN, 0, 0, 0, 0);
-                mouse_event(MOUSEEVENTF_RIGHTUP, 0, 0, 0, 0);
-            }
+        [StructLayout(LayoutKind.Sequential)]
+        private struct MOUSEINPUT
+        {
+            public int dx;
+            public int dy;
+            public uint mouseData;
+            public uint dwFlags;
+            public uint time;
+            public IntPtr dwExtraInfo;
+        }
 
-            if (repeatAmount > 0)
-            {
-                clickCount++;
-            }
+        private const int INPUT_MOUSE = 0;
+        private const int MOUSEEVENTF_LEFTDOWN = 0x0002;
+        private const int MOUSEEVENTF_LEFTUP = 0x0004;
+        private const int MOUSEEVENTF_MIDDLEDOWN = 0x0020;
+        private const int MOUSEEVENTF_MIDDLEUP = 0x0040;
+        private const int MOUSEEVENTF_RIGHTDOWN = 0x0008;
+        private const int MOUSEEVENTF_RIGHTUP = 0x0010;
 
-            Thread.Sleep(clickInterval);
+        private static bool _isAutoClickerRunning;
+        private static Thread _autoClickerThread;
+
+        public static int clickInterval = 1000; // Milliseconds
+        public static int repeatAmount = 0; // How many clicks
+        public static int mouseButton = 0; // 0 = Left, 1 = Middle, 2 = Right
+
+        public static bool IsAutoClickerRunning => _isAutoClickerRunning;
+
+        public static void StartAutoClicker()
+        {
+            _isAutoClickerRunning = true;
+            _autoClickerThread = new Thread(AutoClickerThread);
+            _autoClickerThread.Start();
+        }
+
+        public static void StopAutoClicker()
+        {
+            _isAutoClickerRunning = false;
+            _autoClickerThread.Join();
+        }
+
+        private static async void AutoClickerThread()
+        {
+            var clickCount = 0;
+
+            while (_isAutoClickerRunning)
+            {
+                if (clickCount >= repeatAmount && repeatAmount != 0)
+                {
+                    StopAutoClicker();
+                    break;
+                }
+
+                INPUT[] inputs = new INPUT[2];
+                inputs[0] = CreateMouseEvent(MOUSEEVENTF_LEFTDOWN);
+                inputs[1] = CreateMouseEvent(MOUSEEVENTF_LEFTUP);
+
+                SendInput((uint)inputs.Length, inputs, Marshal.SizeOf(typeof(INPUT)));
+
+                if (repeatAmount > 0)
+                {
+                    clickCount++;
+                }
+
+                await Task.Delay(clickInterval);
+
+            }
+        }
+
+        private static INPUT CreateMouseEvent(uint flags)
+        {
+            return new INPUT
+            {
+                type = INPUT_MOUSE,
+                mi = new MOUSEINPUT
+                {
+                    dwFlags = flags,
+                    time = 0,
+                    dwExtraInfo = IntPtr.Zero
+                }
+            };
         }
     }
 }
