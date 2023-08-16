b5c49d2ed862c1e36f2bb30194ccc4b672c7c7f5
RyanLua <80087248+RyanLua@users.noreply.github.com>
2023-08-16 11:02:48 -0700

    Add hotkey helper placeholder

---
+++ src/Fluent Auto Clicker/Helpers/BindHotkeyHelper.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fluent_Auto_Clicker.Helpers;
+internal class BindHotkeyHelper
+{
+    private static void ListenForKey()
+    {
+    }
+
+    private static void UnlistenForKey()
+    {
+    }
+
+    private static void BindKey()
+    {
+    }
+
+    private void UnbindKey()
+    {
+    }
+}
+
