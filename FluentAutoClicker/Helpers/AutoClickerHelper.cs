e281a4aaa383c12c1f7d099fcb7fa5d5cff6e5a9
Ryan Luu <ryan.luu@ryanluu.dev>
2024-05-21 22:51:11 -0700

    Merge new version

---
+++ FluentAutoClicker/Helpers/AutoClickerHelper.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using System.Threading;
+using System.Timers;
+
+namespace FluentAutoClicker.Helpers;
+
+public static class AutoClickerHelper
+{
+    [DllImport("user32.dll", SetLastError = true)]
+    private static extern uint SendInput(uint nInputs, INPUT[] pInputs, int cbSize);
+
+    [StructLayout(LayoutKind.Sequential)]
+    private struct INPUT
+    {
+        public int type;
+        public INPUT_MOUSE mi;
+    }
+
+    [StructLayout(LayoutKind.Sequential)]
+    private struct INPUT_MOUSE
+    {
+        public int dx;
+        public int dy;
+        public uint mouseData;
+        public uint dwFlags;
+        public uint time;
+        public IntPtr dwExtraInfo;
+    }
+
+    [Flags]
+    internal enum MOUSEEVENTF : uint
+    {
+        LEFTDOWN = 0x0002,
+        LEFTUP = 0x0004,
+        RIGHTDOWN = 0x0008,
+        RIGHTUP = 0x0010,
+        MIDDLEDOWN = 0x0020,
+        MIDDLEUP = 0x0040,
+    }
+
+    private static bool _isAutoClickerRunning;
+    private static Thread? _autoClickerThread;
+
+    public static int clickInterval = 1000; // Milliseconds
+    public static int repeatAmount = 0; // How many clicks
+    public static int mouseButton = 0; // 0 = Left, 1 = Middle, 2 = Right
+
+    public static bool IsAutoClickerRunning => _isAutoClickerRunning;
+
+    public static void StartAutoClicker()
+    {
+        _isAutoClickerRunning = true;
+        _autoClickerThread = new Thread(AutoClickerThread);
+        _autoClickerThread.Start();
+    }
+
+    public static void StopAutoClicker()
+    {
+        _isAutoClickerRunning = false;
+        _autoClickerThread?.Join();
+    }
+
+    private static async void AutoClickerThread()
+    {
+        var clickCount = 0;
+
+        while (_isAutoClickerRunning)
+        {
+            if (clickCount >= repeatAmount && repeatAmount != 0)
+            {
+                StopAutoClicker();
+                break;
+            }
+
+            if (mouseButton == 0)
+            {
+                MouseEvent(0, 0, (uint)MOUSEEVENTF.LEFTDOWN, 0, 0, IntPtr.Zero);
+                MouseEvent(0, 0, (uint)MOUSEEVENTF.LEFTUP, 0, 0, IntPtr.Zero);
+            }
+            else if (mouseButton == 1)
+            {
+                MouseEvent(0, 0, (uint)MOUSEEVENTF.MIDDLEDOWN, 0, 0, IntPtr.Zero);
+                MouseEvent(0, 0, (uint)MOUSEEVENTF.MIDDLEUP, 0, 0, IntPtr.Zero);
+            }
+            else if (mouseButton == 2)
+            {
+                MouseEvent(0, 0, (uint)MOUSEEVENTF.RIGHTDOWN, 0, 0, IntPtr.Zero);
+                MouseEvent(0, 0, (uint)MOUSEEVENTF.RIGHTUP, 0, 0, IntPtr.Zero);
+            }
+
+            if (repeatAmount > 0)
+            {
+                clickCount++;
+            }
+
+            await Task.Delay(clickInterval);
+        }
+    }
+
+    private static void MouseEvent(int dx, int dy, uint dwFlags, uint dwData, uint time, nint dwExtraInfo)
+    {
+        INPUT[] inputs = new INPUT[2];
+        inputs[0] = MOUSEINPUT(dx, dy, dwData, dwFlags, time, dwExtraInfo);
+        inputs[1] = MOUSEINPUT(dx, dy, dwData, dwFlags, time, dwExtraInfo);
+
+        SendInput((uint)inputs.Length, inputs, Marshal.SizeOf(typeof(INPUT)));
+    }
+
+    private static INPUT MOUSEINPUT(int DX, int DY, uint MOUSEDATA, uint DWFLAGS, uint TIME, nint DWEXTRAINFO)
+    {
+        return new INPUT
+        {
+            type = 0, // INPUT_MOUSE
+            mi = new INPUT_MOUSE
+            {
+                dx = DX,
+                dy = DY,
+                mouseData = MOUSEDATA,
+                dwFlags = DWFLAGS,
+                time = TIME,
+                dwExtraInfo = DWEXTRAINFO
+            }
+        };
+    }
+}
--- FluentAutoClicker/Views/MainPage.xaml.cs
+++ FluentAutoClicker/Views/MainPage.xaml.cs
@@ -1,19 +1,129 @@
-﻿using FluentAutoClicker.ViewModels;
-
+﻿using System.Diagnostics;
+using FluentAutoClicker.Helpers;
+using Microsoft.UI.Xaml;
 using Microsoft.UI.Xaml.Controls;
+using Microsoft.UI.Xaml.Controls.Primitives;
+using Windows.Globalization.NumberFormatting;
 
 namespace FluentAutoClicker.Views;
 
 public sealed partial class MainPage : Page
 {
-    public MainViewModel ViewModel
-    {
-        get;
-    }
-
     public MainPage()
     {
-        ViewModel = App.GetService<MainViewModel>();
         InitializeComponent();
+        StartClicker.Checked += StartClicker_Checked;
+        StartClicker.Unchecked += StartClicker_Unchecked;
+    }
+
+    private void ButtonType_SelectionChanged(object sender, SelectionChangedEventArgs e)
+    {
+        var comboBox = (ComboBox)sender;
+        var mouseButtonIndex = comboBox.SelectedIndex;
+
+        AutoClickerHelper.mouseButton = mouseButtonIndex;
+
+        Debug.WriteLine($"Mouse Button: {AutoClickerHelper.mouseButton}");
+    }
+
+    private void SetClicker_Interval()
+    {
+        var hours = Convert.ToInt32(Interval_Hours.Value);
+        var minutes = Convert.ToInt32(Interval_Minutes.Value);
+        var seconds = Convert.ToInt32(Interval_Seconds.Value);
+        var milliseconds = Convert.ToInt32(Interval_Milliseconds.Value);
+
+        var totalTimeInMilliseconds = ((hours * 60 + minutes) * 60 + seconds) * 1000 + milliseconds;
+
+        AutoClickerHelper.clickInterval = totalTimeInMilliseconds;
+
+        Debug.WriteLine($"Interval Time (Milliseconds): {AutoClickerHelper.clickInterval}");
+    }
+
+    private void IntervalNumberBox_ValueChanged(NumberBox sender, NumberBoxValueChangedEventArgs args)
+    {
+        SetClicker_Interval();
+    }
+
+    private void RepeatTypeRadioButtons_SelectionChanged(object sender, RoutedEventArgs e)
+    {
+        SetClicker_Repeat();
+    }
+
+    private void RepeatTypeNumberBox_ValueChanged(NumberBox sender, NumberBoxValueChangedEventArgs args)
+    {
+        SetClicker_Repeat();
+        if (RepeatType_Clicks.Value != 100)
+        {
+            RepeatTypeRadioButtons.SelectedIndex = 0;
+        }
+    }
+
+    private void SetClicker_Repeat()
+    {
+        var repeatCount = Convert.ToInt32(RepeatType_Clicks.Value);
+        var repeatTypeIndex = RepeatTypeRadioButtons.SelectedIndex;
+
+        if (repeatTypeIndex == 0)
+        {
+            AutoClickerHelper.repeatAmount = repeatCount;
+        }
+        else
+        {
+            AutoClickerHelper.repeatAmount = 0;
+        }
+
+        Debug.WriteLine($"Repeat Count: {AutoClickerHelper.repeatAmount}");
+    }
+
+
+    private void StartClicker_Checked(object sender, RoutedEventArgs e)
+    {
+        Thread.Sleep(1000);
+        AutoClickerHelper.StartAutoClicker();
+        SetClicker_Interval();
+        SetClicker_Repeat();
+
+        Debug.WriteLine($"Auto Clicker Running: {AutoClickerHelper.IsAutoClickerRunning}");
+    }
+
+    private void StartClicker_Unchecked(object sender, RoutedEventArgs e)
+    {
+        AutoClickerHelper.StopAutoClicker();
+        Debug.WriteLine($"Auto Clicker Stopped: {AutoClickerHelper.IsAutoClickerRunning}");
+    }
+
+    private void KeyboardAccelerator_Invoked(Microsoft.UI.Xaml.Input.KeyboardAccelerator sender, Microsoft.UI.Xaml.Input.KeyboardAcceleratorInvokedEventArgs args)
+    {
+        if (AutoClickerHelper.IsAutoClickerRunning)
+        {
+            AutoClickerHelper.StopAutoClicker();
+            Debug.WriteLine($"Auto Clicker Stopped: {AutoClickerHelper.IsAutoClickerRunning}");
+        }
+        else
+        {
+            AutoClickerHelper.StartAutoClicker();
+            SetClicker_Interval();
+            SetClicker_Repeat();
+
+            Debug.WriteLine($"Auto Clicker Running: {AutoClickerHelper.IsAutoClickerRunning}");
+        }
+    }
+
+    private async void HotkeyButton_Click(object sender, RoutedEventArgs e)
+    {
+        ContentDialog dialog = new ContentDialog();
+
+        // XamlRoot must be set in the case of a ContentDialog running in a Desktop app
+        dialog.XamlRoot = this.XamlRoot;
+        dialog.Style = Application.Current.Resources["DefaultContentDialogStyle"] as Style;
+        dialog.Title = "Set hotkey";
+        dialog.PrimaryButtonText = "Save";
+        dialog.SecondaryButtonText = "Reset";
+        dialog.CloseButtonText = "Cancel";
+        dialog.DefaultButton = ContentDialogButton.Primary;
+        dialog.Content = "Press any key to bind it as a hotkey.";
+
+        var result = await dialog.ShowAsync();
     }
 }
