ef9d7c48d6db8ffe54029a45f1f299f1b9192d38
Ryan Luu <ryan.luu@ryanluu.dev>
2024-08-15 00:17:14 -0700

    Standardize project

--- FluentAutoClicker/App.xaml.cs
+++ FluentAutoClicker/App.xaml.cs
@@ -1,33 +1,50 @@
 ﻿using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Controls;
+using Microsoft.UI.Xaml.Controls.Primitives;
+using Microsoft.UI.Xaml.Data;
+using Microsoft.UI.Xaml.Input;
+using Microsoft.UI.Xaml.Media;
+using Microsoft.UI.Xaml.Navigation;
+using Microsoft.UI.Xaml.Shapes;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.ApplicationModel;
+using Windows.ApplicationModel.Activation;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
 
 // To learn more about WinUI, the WinUI project structure,
 // and more about our project templates, see: http://aka.ms/winui-project-info.
 
-namespace FluentAutoClicker;
-
-/// <summary>
-///     Provides application-specific behavior to supplement the default Application class.
-/// </summary>
-public partial class App
+namespace FluentAutoClicker
 {
-    private Window _mWindow;
-
     /// <summary>
-    ///     Initializes the singleton application object.  This is the first line of authored code
-    ///     executed, and as such is the logical equivalent of main() or WinMain().
+    /// Provides application-specific behavior to supplement the default Application class.
     /// </summary>
-    public App()
+    public partial class App : Application
     {
-        InitializeComponent();
-    }
+        /// <summary>
+        /// Initializes the singleton application object.  This is the first line of authored code
+        /// executed, and as such is the logical equivalent of main() or WinMain().
+        /// </summary>
+        public App()
+        {
+            this.InitializeComponent();
+        }
 
-    /// <summary>
-    ///     Invoked when the application is launched.
-    /// </summary>
-    /// <param name="args">Details about the launch request and process.</param>
-    protected override void OnLaunched(LaunchActivatedEventArgs args)
-    {
-        _mWindow = new MainWindow();
-        _mWindow.Activate();
+        /// <summary>
+        /// Invoked when the application is launched.
+        /// </summary>
+        /// <param name="args">Details about the launch request and process.</param>
+        protected override void OnLaunched(Microsoft.UI.Xaml.LaunchActivatedEventArgs args)
+        {
+            m_window = new MainWindow();
+            m_window.Activate();
+        }
+
+        private Window m_window;
     }
 }
