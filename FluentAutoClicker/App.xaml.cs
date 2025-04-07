0c83f951bdd481f5e1a3dde17a6b2782f19d937f
Ryan Luu <ryan.luu@ryanluu.dev>
2025-04-06 21:08:50 -0700

    Use PInvoke for efficiency mode

--- FluentAutoClicker/App.xaml.cs
+++ FluentAutoClicker/App.xaml.cs
@@ -15,8 +15,9 @@
 // You should have received a copy of the GNU Affero General Public License
 // along with Fluent Auto Clicker. If not, see <https://www.gnu.org/licenses/>.
 
-using FluentAutoClicker.Helpers;
 using Microsoft.UI.Xaml;
+using Windows.Win32;
+using Windows.Win32.System.Threading;
 
 // To learn more about WinUI, the WinUI project structure,
 // and more about our project templates, see: http://aka.ms/winui-project-info.
@@ -40,7 +41,22 @@ public partial class App
     public App()
     {
         InitializeComponent();
-        EfficiencyModeHelper.SetEfficiencyMode(true);
+
+        PROCESS_POWER_THROTTLING_STATE PowerThrottling = new()
+        {
+            ControlMask = PInvoke.PROCESS_POWER_THROTTLING_EXECUTION_SPEED,
+            StateMask = PInvoke.PROCESS_POWER_THROTTLING_EXECUTION_SPEED,
+            Version = PInvoke.PROCESS_POWER_THROTTLING_CURRENT_VERSION
+        };
+
+        // Set the process power throttling to efficiency mode
+        unsafe
+        {
+            PInvoke.SetProcessInformation(PInvoke.GetCurrentProcess(), PROCESS_INFORMATION_CLASS.ProcessPowerThrottling, &PowerThrottling, (uint)sizeof(PROCESS_POWER_THROTTLING_STATE));
+        }
+
+        // Set the process priority to idle
+        _ = PInvoke.SetPriorityClass(PInvoke.GetCurrentProcess(), PROCESS_CREATION_FLAGS.IDLE_PRIORITY_CLASS);
     }
 
     /// <summary>
